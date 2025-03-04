4ecfdf958f70cfa83528494ea5f091979dbc90f7
Abu AlRahman <abumohd.zalrahman@gmail.com>
2025-03-04 07:28:01 +0600

    removed a few dependencies from init file

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,4 @@
-__all__ = ['utils', 'functions']
-
 from .functions import *
-from .process_functions import *
-from .signal import *
-from .Trial import *
 from .file_ops import *
 from .session_metadata import *
 from .plotting import *
